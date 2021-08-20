756f7178da75c8d5d97f09dc24a2af2e5ef9d193
sannimdev <inegg.apps@gmail.com>
2021-08-20 22:53:24 +0900

    23강, 24강: computed (Vue3), 검색 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="container">
         <h2>To-Do List</h2>
+        <input class="form-control" type="text" v-model="searchText" placeholder="Search" />
+        <hr />
         <TodoSimpleForm @add-todo="addTodo" />
-        <div v-if="!todos.length">
-            추가된 Todo가 없습니다.
+        <div v-if="!filteredTodos.length">
+            There is nothing to display
         </div>
-        <TodoList :todos="todos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" />
+        <TodoList :todos="filteredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" />
     </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
 
@@ -50,12 +52,25 @@ export default {
             todos.value[index].completed = !todos.value[index].completed;
         };
 
+        const searchText = ref('');
+
+        const filteredTodos = computed(() => {
+            if (searchText.value) {
+                return todos.value.filter((todo) => {
+                    return todo.subject.includes(searchText.value);
+                });
+            }
+            return todos.value;
+        });
+
         return {
             todos,
             todoStyle,
             addTodo,
             deleteTodo,
             toggleTodo,
+            searchText,
+            filteredTodos,
         };
     },
 };
