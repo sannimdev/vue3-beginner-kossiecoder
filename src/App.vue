6dbb7b5a124bf31ea53a13a2789b2a86006eb21a
sannimdev <inegg.apps@gmail.com>
2021-09-22 18:24:07 +0900

    42강: NavBar 추가하기

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Kossie Coder</a>
+
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Todos <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </nav>
     <router-view />
 </template>
 
@@ -6,9 +15,4 @@
 export default {};
 </script>
 
-<style>
-.todo {
-    color: gray;
-    text-decoration: line-through;
-}
-</style>
+<style></style>
