62359c999bfbd5535ad4f50f55ee166da9f8936b
sannimdev <inegg.apps@gmail.com>
2021-09-24 20:42:26 +0900

    45강: To-Do 수정 페이지 route 추가하기

---
+++ src/pages/todos/_id.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Todo Page</div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+export default {
+    setup() {
+        const route = useRoute();
+
+        console.log(route, route.params);
+    },
+};
+</script>
+
+<style></style>
