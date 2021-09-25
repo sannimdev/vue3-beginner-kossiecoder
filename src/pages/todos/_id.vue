562b6f49822d228773a8b64bdc7e2f7a53b634f1
sannimdev <inegg.apps@gmail.com>
2021-09-25 12:24:51 +0900

    48강: To-Do 페이지 컴포넌트 만들기 1

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -1,14 +1,40 @@
 <template>
-    <div>Todo Page</div>
+    <h1>To-do Page</h1>
+    <div v-if="loading">
+        Loading...
+    </div>
+    <form v-else>
+        <div class="form-group">
+            <label> Todo Subject</label>
+            <input v-model="todo.subject" type="text" class="form-control" />
+        </div>
+        <button class="btn btn-primary">Save</button>
+    </form>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 import { useRoute } from 'vue-router';
+import axios from 'axios';
+
 export default {
     setup() {
         const route = useRoute();
+        const todo = ref(null);
+        const loading = ref(true);
 
-        console.log(route, route.params);
+        const getTodo = async () => {
+            const res = await axios.get('http://localhost:3000/todos/' + route.params.id);
+
+            todo.value = res.data;
+            loading.value = false;
+        };
+
+        getTodo();
+        return {
+            todo,
+            loading,
+        };
     },
 };
 </script>
