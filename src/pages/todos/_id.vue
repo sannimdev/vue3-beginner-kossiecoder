912206beb151db9df526a63305811afd5feb8cf1
sannimdev <inegg.apps@gmail.com>
2021-09-25 14:36:51 +0900

    49강: To-Do 페이지 컴포넌트 만들기 2

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -4,22 +4,41 @@
         Loading...
     </div>
     <form v-else>
-        <div class="form-group">
-            <label> Todo Subject</label>
-            <input v-model="todo.subject" type="text" class="form-control" />
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label>Todo Subject</label>
+                    <input v-model="todo.subject" type="text" class="form-control" />
+                </div>
+            </div>
+            <div class="col-6">
+                <label>Completed</label>
+                <div>
+                    <button
+                        type="button"
+                        class="btn"
+                        :class="todo.completed ? 'btn-success' : 'btn-danger'"
+                        @click="toggleTodoStatus"
+                    >
+                        {{ todo.completed ? 'Completed' : 'Incomplete' }}
+                    </button>
+                </div>
+            </div>
         </div>
-        <button class="btn btn-primary">Save</button>
+        <button type="submit" class="btn btn-primary">Save</button>
+        <button class="btn btn-outline-dark ml-2" @click="moveTodoListPage">Cancel</button>
     </form>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
 
 export default {
     setup() {
         const route = useRoute();
+        const router = useRouter();
         const todo = ref(null);
         const loading = ref(true);
 
@@ -30,10 +49,22 @@ export default {
             loading.value = false;
         };
 
+        const toggleTodoStatus = () => {
+            todo.value.completed = !todo.value.completed;
+        };
+
+        const moveTodoListPage = () => {
+            router.push({
+                name: 'Todos',
+            });
+        };
+
         getTodo();
         return {
             todo,
             loading,
+            toggleTodoStatus,
+            moveTodoListPage,
         };
     },
 };
